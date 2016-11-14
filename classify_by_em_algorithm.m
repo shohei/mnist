55ff94250534ce6d33a0b498ed9ee9baba2ce65e
Shohei Aoki <shoaok@gmail.com>
2016-11-14 20:11:25 +0900

    add EM algorithm for classifying handwriting dataset

--- classify_by_em_algorithm.m
+++ classify_by_em_algorithm.m
@@ -7,14 +7,18 @@ global img;
 global img2;
 open_and_read_mnist();
 binarize();
+global img3;
+prepare_serialized_image();
 
 global uk;
 init_generator();
 global pk;
 init_pickup_probability();
 
+global gamma_nk;
 compute_generation_probability();
 recreate_generator();
+disp 'comutation done';
 
     function open_and_read_mnist
         fid=fopen('train-images-idx3-ubyte','r','b')
@@ -35,15 +39,19 @@ recreate_generator();
         img2(img2>thresh)=0;
     end
 
+    function prepare_serialized_image
+        img3 = reshape(img2,[28*28 60000]);
+    end
+
     function init_generator
-        for idx=1:10
+        for k=1:10
             u = [];
             for jdx=1:784
                 u(end+1) = round(rand(1));
             end
-            u = reshape(u,[28,28]);
-            uk(end+1)=u;
+            uk(:,end+1)=u;
         end
+        uk = reshape(uk,[28*28 10]);
     end
 
     function init_pickup_probability
@@ -52,13 +60,33 @@ recreate_generator();
         end
     end
 
-    function compute_generation_probability
-        gamma_nk
+    function compute_generation_probability        
+        for k=1:10
+            puk = [];
+            for n=1:60000
+                puk(k,n)=1;
+                for pixel=1:28*28
+                    puk(k,n) = puk(k,n) * uk(pixel,k)^img3(pixel,n) * (1-uk(pixel,k))^(1-img3(pixel,n));
+                end
+            end
+            denominator = 0;
+            denominator = denominator + pk(k)*puk(k,n);
+            gamma_nk(k,n) = pk(k)*puk(k,n)/denominator;
+            disp( sprintf( 'cycle %d done!', k ) );
+        end    
     end
 
     function recreate_generator
+        numerator = 0;
+        denominator = 0;
+        for k=1:10
+            for n=1:60000
+                numerator = numerator + gamma_nk(k,n)*img3(:,n);
+                denominator = denominator + gamma_nk(k,n);
+            end
+            uk(:,k) = numerator / denominator;
+            pk(k) = denominator/60000;
+        end                
     end
-
-
 
 end
